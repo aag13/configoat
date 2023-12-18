91a5129a7a3d32f36fd11c29d6621c18c97f7fd1
Asadullah Al Galib <asadullah.galib@bracits.com>
2023-12-18 17:17:08 +0600

    release: version '0.1.6'

--- setup.py
+++ setup.py
@@ -51,6 +51,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/aag13/configoat',
-    version='0.1.5',
+    version='0.1.6',
     zip_safe=False,
 )
